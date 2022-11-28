ecdc4ebb18813b544ad30fb8f662f19f8d891904
Tomasz Cyndler <tomasz.cyndler@gmail.com>
2022-11-28 18:01:35 +0100

    Initial commit

---
+++ 10kolejnych.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+
+int main()
+{
+    int i,suma;
+    i=0;
+    suma=0;
+    while(i<10)
+    {
+
+        suma=suma+i;
+        i=i+1;
+        cout<<suma<<endl;
+    }
+}
