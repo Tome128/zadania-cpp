a79d97f96ba2a315779a36cc82c49c45df28bc76
Tomasz Cyndler <tomasz.cyndler@gmail.com>
2022-11-28 18:02:58 +0100

    formatting

--- 10kolejnych.cpp
+++ 10kolejnych.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include<math.h>
+
 using namespace std;
 
 
-int main()
-{
-    int i,suma;
-    i=0;
-    suma=0;
-    while(i<10)
-    {
+int main() {
+    int i, suma;
+    i = 0;
+    suma = 0;
+    while (i < 10) {
 
-        suma=suma+i;
-        i=i+1;
-        cout<<suma<<endl;
+        suma = suma + i;
+        i = i + 1;
+        cout << suma << endl;
     }
 }
